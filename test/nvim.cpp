a36a381d01f67cb0a0976b33fc4035009d545e5e
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-05-25 23:15:03 -0600

    New update

---
+++ test/nvim.cpp
@@ -0,0 +1,14 @@
+/**
+ *   author:  josuerom
+ *   created: 26/05/23 23:14
+**/
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        std::cout << "nvim v0."<<t<<".0 by josuerom!" << '\n';
+    }
+    return 0;
+}
